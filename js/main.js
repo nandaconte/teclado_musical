03cb1ae8c4e75e0785ae0a1e1383852c11b6b64c
nandaconte <fernanda.accsilva@senacsp.edu.br>
2023-11-12 13:35:07 -0300

    12-11-2023

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function tocaSom (idElementAudio) {
+
+    document.querySelector(idElementAudio).play(); 
+ }
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//para
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];    
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+}
+
